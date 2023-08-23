31e01686c7944f89ed5db79d21e4b8fc129dafda
Bernardo Martorano <bernardormartorano@gmail.com>
2023-08-22 21:58:18 -0300

    req05

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
-from .views import inicial_page
+from .views import inicial_page, details_page
 from django.urls import path
 
 
 urlpatterns = [
-    path("", inicial_page, name="home-page")
+    path("", inicial_page, name="home-page"),
+    path("news/<int:id>", details_page, name="news-details-page")
 ]
--- news/views.py
+++ news/views.py
@@ -5,3 +5,9 @@ from .models.news_model import News
 def inicial_page(request):
     context = {"news": News.objects.all()}
     return render(request, "home.html", context)
+
+
+def details_page(request, id):
+    news = News.objects.get(id=id)
+    context = {"news": news}
+    return render(request, "news_details.html", context)
